02d11785919aa99636f9501462f29fdc31176416
Sharmila K <sharmilakanagaraj01@gmail.com>
2026-03-08 15:53:26 +0530

    fix eslint warnings

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,20 +1,8 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui";
-import styles from "./page.module.css";
-
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
 
 export default function Page() {
   return (
-    <main className={styles.main}>
-      <h1>Docs App Ready 📚</h1>
-
-      <div style={{ marginTop: "20px" }}>
-        <Button>Shared UI Button 🚀</Button>
-      </div>
-    </main>
+    <div>
+      Documentation Page
+    </div>
   );
 }
